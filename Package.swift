03bb8bb430d60f1832a20198d1bf229c8886c42b
PotatoArtie <98959780+PotatoArtie@users.noreply.github.com>
2023-03-21 19:16:05 +0900

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "KoreanCurrencyKit",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -24,5 +27,8 @@ let package = Package(
         .testTarget(
             name: "KoreanCurrencyKitTests",
             dependencies: ["KoreanCurrencyKit"]),
+    ],
+    swiftLanguageVersions: [
+        .v5
     ]
 )
